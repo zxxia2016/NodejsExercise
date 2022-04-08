b5e8c1d1560e7fc774e58eca787f4d38a7679cbc
zxxia2016 <zxxia2016@gmail.com>
2022-04-08 19:24:19 +0800

    读表完成

--- read_big_xlsx/app.js
+++ read_big_xlsx/app.js
@@ -50,10 +50,12 @@ else {
  * 读取3月配置表
  */
 //----------------------------------------------------------------------------------
+
 let month3RowCount = 0;
-cfgMonth = 1;
+cfgMonth = 3;
 const pathMonth3 = `./data/${cfgMonth}.xlsx`;
 let beginMonth3 = 0;
+let timer3 = null;
 let f3 = function () {
     return new Promise((resolve, reject) => {
         new XLSX().extract(pathMonth3, { sheet_id: 1 }) // or sheet_name or sheet_nr
@@ -61,6 +63,9 @@ let f3 = function () {
                 // console.log('sheet', sheet);  //sheet is array [sheetname, sheetid, sheetnr]
                 console.log(`找到 {${cfgMonth}} 月数据成功`);
                 beginMonth3 = new Date().getTime();
+                timer3 = setInterval(() => {
+                    console.log(`month3RowCount: ${month3RowCount}`);
+                }, 1000);
             })
             .on('row', function (row) {
                 //搜索词
@@ -68,7 +73,6 @@ let f3 = function () {
                 if (!words) {
                     return;
                 }
-                month3RowCount++;
                 //搜索词单词数
                 const wordCount = words.split(" ").length;
                 //单月出现次数（取搜索词重复）
@@ -77,9 +81,9 @@ let f3 = function () {
                 if (isNaN(r)) {
                     return;
                 }
+                month3RowCount++;
                 // console.log(`找到第 {${month3RowCount}} 条数据`);
 
-                //todo先查询，再插入
                 const sql = `select * from ${tbNme} where SearchTerm = ?`;
                 sqliteDB.getDataPromise(sql, [words]).then(function (rowData) {
                     if (rowData) {
@@ -133,14 +137,141 @@ let f3 = function () {
             })
             .on('end', function (err) {
                 console.log(`读取${cfgMonth}月表时间为：${(new Date().getTime() - beginMonth3) / 1000 / 3600} 小时`)
-
+                console.log(`读取总数为: ${month3RowCount}`);
                 resolve();
             });
     });
 }
-const timer = setInterval(() => {
-    console.log(`month3RowCount: ${month3RowCount}`);
-}, 3000);
+
+
+let month2RowCount = 0;
+
+// 读取2月
+let month2 = (cfgMonth + 12 - 1) % 12;
+if (month2 == 0) {
+    month2 = 12;
+}
+const pathMonth2 = `./data/${month2}.xlsx`;
+let beginMonth2 = 0;
+let timer2 = null;
+const f2 = function () {
+    return new Promise((resolve, reject) => {
+        new XLSX().extract(pathMonth2, { sheet_id: 1 }) // or sheet_name or sheet_nr
+            .on('sheet', function (sheet) {
+                // console.log('sheet', sheet);  //sheet is array [sheetname, sheetid, sheetnr]
+                console.log(`找到 {${month2}} 月数据成功`);
+                beginMonth2 = new Date().getTime();
+                timer2 = setInterval(() => {
+                    console.log(`month2RowCount: ${month2RowCount}`);
+                }, 1000);
+            })
+            .on('row', function (row) {
+                // console.log('row', row);  //row is a array of values or []
+                //搜索词
+                const words = row[1];
+                if (!words) {
+                    return;
+                }
+                const rank = row[2];
+                const r = Number(rank);
+                if (isNaN(r)) {
+                    return;
+                }
+                month2RowCount++;
+
+                const sql = `select * from ${tbNme} where SearchTerm = ?`;
+                sqliteDB.getDataPromise(sql, [words]).then(function (rowData) {
+                    if (rowData) {
+                        // 更新数量
+                        const sql = `update ${tbNme} set SearchCount2 = ?, Rank2 = ?,
+                        X2ClickedAsin_1 = ?, X2Product_1 = ?, , X2ClickShare_1 = ?,
+                        X2ConversionShare_1 = ?, X2ClickedAsin_2 = ?, X2Product_2 = ?,
+                        X2ClickShare_2 = ?,
+                        X2ConversionShare_2 = ?,X2ClickedAsin_3 = ?,X2Product_3 = ?,
+                        X2ClickShare_3 = ?,X2ConversionShare_3 = ?,where SearchTerm = ?`;
+                        const array = [++rowData.SearchCount2, rank, row[3], row[4], row[5],
+                        row[6], row[7], row[8], row[9], row[10], row[11], row[12],
+                        row[13], row[14], words];
+                        sqliteDB.runSql(sql, array);
+                    }
+                })
+
+            })
+            .on('error', function (err) {
+                console.error('error', err);
+            })
+            .on('end', function (err) {
+                console.log(`读取${month2}月表时间为：${(new Date().getTime() - beginMonth2) / 1000 / 3600} 小时`);
+                console.log(`读取总数为: ${month2RowCount}`);
+                resolve();
+            });
+    });
+};
+
+// 读取1
+let month1RowCount = 0;
+let month1 = (cfgMonth + 12 - 2) % 12;
+if (month1 == 0) {
+    month1 = 12;
+}
+const pathMonth1 = `./data/${month1}.xlsx`;
+let beginMonth1 = 0;
+let timer1 = null;
+const f1 = function () {
+    return new Promise((resolve, reject) => {
+        new XLSX().extract(pathMonth1, { sheet_id: 1 }) // or sheet_name or sheet_nr
+            .on('sheet', function (sheet) {
+                // console.log('sheet', sheet);  //sheet is array [sheetname, sheetid, sheetnr]
+                console.log(`找到 {${month1}} 月数据成功`);
+                timer1 = setInterval(() => {
+                    console.log(`month2RowCount: ${month2RowCount}`);
+                }, 1000);
+                beginMonth1 = new Date().getTime();
+            })
+            .on('row', function (row) {
+                // console.log('row', row);  //row is a array of values or []
+                month1RowCount++;
+                const words = row[1];
+                if (!words) {
+                    return;
+                }
+                const rank = row[2];
+                const r = Number(rank);
+                if (isNaN(r)) {
+                    return;
+                }
+                month1RowCount++;
+
+                const sql = `select * from ${tbNme} where SearchTerm = ?`;
+                sqliteDB.getDataPromise(sql, [words]).then(function (rowData) {
+                    if (rowData) {
+                        // 更新数量
+                        const sql = `update ${tbNme} set SearchCount1 = ?, Rank1 = ?,
+                        X1ClickedAsin_1 = ?, X1Product_1 = ?, , X1ClickShare_1 = ?,
+                        X1ConversionShare_1 = ?, X1ClickedAsin_2 = ?, X1Product_2 = ?,
+                        X1ClickShare_2 = ?,
+                        X1ConversionShare_2 = ?,X1ClickedAsin_3 = ?,X1Product_3 = ?,
+                        X1ClickShare_3 = ?,X1ConversionShare_3 = ?,where SearchTerm = ?`;
+                        const array = [++rowData.SearchCount1, rank, row[3], row[4], row[5],
+                        row[6], row[7], row[8], row[9], row[10], row[11], row[12],
+                        row[13], row[14], words];
+                        sqliteDB.runSql(sql, array);
+                    }
+                })
+
+            })
+            .on('error', function (err) {
+                console.error('error', err);
+            })
+            .on('end', function (err) {
+                console.log(`读取${month1}月表时间为：${(new Date().getTime() - beginMonth1) / 1000 / 3600} 小时`);
+                console.log(`读取总数为: ${month1RowCount}`);
+                resolve();
+            });
+    });
+}
+
+
 
 if (cfgMonth) {
     const file = `./tmp/data.db`;
@@ -197,8 +328,18 @@ if (cfgMonth) {
             )`;
             return sqliteDB.createTable(createTableSql);
         })
-        .then(f3).then(function (data) {
-            clearInterval(timer);
+        .then(f3).then(function () {
+            return new Promise(function () {
+                clearInterval(timer3);
+            })
+        }).then(f2).then(function () {
+            return new Promise(function () {
+                clearInterval(timer2);
+            })
+        }).then(f1).then(function () {
+            return new Promise(function () {
+                clearInterval(timer1);
+            })
         }).catch(function (err) {
             console.error(err);
         })
